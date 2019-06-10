e7bed396c6f647ff9f88369c0fa768b72d8f9155
michaelstsnow <michael.st.snow@gmail.com>
2019-06-10 23:13:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ class Student
       VALUES ()?,?)
       SQL
 
-      DB[:conn].execute(sql,self.id,self.name,self.grade)
+      DB[:conn].execute(sql,self.name,self.grade)
 
       @id=DB[:conn].execute("SELECT last_insert_rowid() FROM songs").flatten
     end
