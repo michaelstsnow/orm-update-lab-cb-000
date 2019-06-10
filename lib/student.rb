c79216a165b38041a33b89ffa28c333a064d11c3
michaelstsnow <michael.st.snow@gmail.com>
2019-06-10 23:22:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,6 +67,11 @@ class Student
   end
 
   def self.find_by_name(name)
+    sql=<<-SQL
+    SELECT * FROM students
+    WHERE name=?
+    SQL
+    DB[:conn].execute(sql,self.name,self.grade,self.id)
   end
 
 
